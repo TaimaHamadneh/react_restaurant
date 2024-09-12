fdfbc908152c8d8aab081335cf140744c9b0f851
TaimaHamadenh <taimanizar45@gmail.com>
2024-09-12 21:19:50 +0300

    feat(Header): Add Header Section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar  from './components/Navbar/Navbar';
+import Header from './container/Header/Header'
 
 function App() {
   return (
     <div>
     <Navbar />
+    <Header />
     </div>
   );
 }
--- src/constants/images.js
+++ src/constants/images.js
@@ -1,10 +1,12 @@
 import icon from '../assets/icon.png';
+import welcome from '../assets/welcome.png';
 
 
 
 
 export default {
     icon,
+    welcome,
  
     
 };
